e4b57fdd54844dcb843cddf543cd9e521d5598e3
toyota-m2k <toyota.mitsuki@gmail.com>
2021-02-28 19:06:17 +0900

    RecyclerViewBindingを使ってみて気づいた不備を修正

--- libBindIt/src/main/java/com/michael/bindit/impl/RecycleViewBinding.kt
+++ libBindIt/src/main/java/com/michael/bindit/impl/RecycleViewBinding.kt
@@ -12,27 +12,36 @@ import com.michael.bindit.impl.list.RecyclerViewAdapter
 import io.reactivex.rxjava3.disposables.Disposable
 
 class RecycleViewBinding<T>(
-        private val list: ObservableList<T>,
-        private val itemViewLayoutId:Int,
-        private val bindView:(Binder, View, T)->Unit
+    val list: ObservableList<T>,
+    val view: RecyclerView
+//        private val itemViewLayoutId:Int,
+//        private val bindView:(Binder, View, T)->Unit
 ) : DisposableImpl() {
 
     override val mode: BindingMode = BindingMode.OneWay
-    var view:RecyclerView? = null
-    var adapter: Disposable? = null
+//    var adapter: Disposable? = null
 
-    fun connect(owner:LifecycleOwner, view:RecyclerView) {
-        view.adapter = RecyclerViewAdapter.Simple(owner,list,itemViewLayoutId,bindView)
-    }
+
+//    fun connect(owner:LifecycleOwner, view:RecyclerView) {
+//        view.adapter = RecyclerViewAdapter.Simple(owner,list,itemViewLayoutId,bindView)
+//    }
 
     override fun cleanup() {
-        val adapter = view?.adapter as? Disposable ?: return
+        val adapter = view.adapter as? Disposable ?: return
         adapter.dispose()
     }
 
     companion object {
-        fun <T> create(owner: LifecycleOwner, view: RecyclerView, list:ObservableList<T>, itemViewLayoutId:Int, bindView:(Binder, View, T)->Unit) : RecycleViewBinding<T> {
-            return RecycleViewBinding(list,itemViewLayoutId,bindView).apply { connect(owner,view) }
+        fun <T> create(owner: LifecycleOwner, view: RecyclerView, list: ObservableList<T>, itemViewLayoutId:Int, bindView:(Binder, View, T)->Unit) : RecycleViewBinding<T> {
+            return RecycleViewBinding(list,view).apply {
+                view.adapter = RecyclerViewAdapter.Simple(owner,list,itemViewLayoutId,bindView)
+            }
         }
+//        fun <T,B> create(owner: LifecycleOwner, view: RecyclerView, list:ObservableList<T>, createView:(parent: ViewGroup, viewType:Int)->B, bind: (binding: B, item:T)->Unit) : RecycleViewBinding<T>
+//        where B: ViewDataBinding {
+//            return RecycleViewBinding(list,view).apply {
+//                view.adapter = RecyclerViewAdapter.SimpleWithDataBinding<T,B>(owner,list,createView,bind)
+//            }
+//        }
     }
 }
