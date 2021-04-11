c4c36b602ea5abfe5e0742048e4afb7eec379cab
toyota-m2k <toyota.mitsuki@gmail.com>
2021-04-11 09:45:00 +0900

    Commandを改善

---
+++ libBindIt/src/main/java/com/michael/bindit/util/CollectionExt.kt
@@ -0,0 +1,38 @@
+package com.michael.bindit.util
+
+import java.util.concurrent.atomic.DoubleAccumulator
+
+// Collection の unchecked cast ワーニングを鎮める拡張API
+// 型が明らかな場合は @Suppress("UNCHECKED_CAST") でもよいだろうが、どこの馬の骨ともわからないものなら、ちゃんとチェックしてやったらどうや、
+// と思って作ってみた。
+
+@Suppress("UNCHECKED_CAST")
+inline fun <reified K, reified V> Map<*,*>.asMapOfType(): Map<K,V>? =
+        if(keys.all { it is K } && values.all { it is V }) this as Map<K,V> else null
+
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T> List<*>.asListOfType(): List<T>? =
+        if(all {it is T}) this as List<T> else null
+
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T> Array<*>.asArrayOfType(): Array<T>? =
+        if(all {it is T}) this as Array<T> else null
+
+fun <T> List<T>.reverse():Iterable<T> {
+    return Iterable {
+        iterator {
+            for (i in count()-1 downTo 0) {
+                yield(get(i)!!)
+            }
+        }
+    }
+}
+
+fun <S,E> Collection<E>.reduce(accumulator:S, fn:(acc:S,element:E)->S): S {
+    var acc = accumulator
+    forEach {
+        fn(acc,it)
+    }
+    return acc
+}
+
